cf2ed33fa70bcd1a438caeb0d56c65a39ea722a8
ll8464 <75961894+ll8464@users.noreply.github.com>
2022-02-02 19:40:47 -0600

    created views and installed bootstrap

--- config.php
+++ config.php
@@ -7,5 +7,5 @@ define("DB_PASS", "Kxh8WipgZWRL8i9");
 define("DB_NAME", "shareboard");
 
 //Define URL
-define("ROOT_PATH", "/");
-define("ROOT_URL", "http://localhost/dev.localhost");
+define("ROOT_PATH", "/dev.localhost/");
+define("ROOT_URL", "http://localhost/dev.localhost/");
--- views/main.php
+++ views/main.php
@@ -1,12 +1,49 @@
 
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shareboard</title>
+	<title>Shareboard</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<!--<link rel="stylesheet" href="assets/css/style.css"> -->
+  <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>assets/css/styles.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-default">
+  <div class="container-fluid container">
     
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div id="navbar" class="collapse navbar-collapse">
+    <a class="navbar-brand" href="#">Shareboard</a> 
+          
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="<?php echo ROOT_URL; ?>">Home</a></li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="<?php echo ROOT_URL; ?>shares">Shares</a></li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="<?php echo ROOT_URL; ?>users/login">Login</a>            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="<?php echo ROOT_URL; ?>users/register">Register</a></li>
+      
+      
+    </div>
+  </div>
+</nav>
+
+    <div class="container">
+
+     <div class="row">
+     	<?php require($view); ?>
+     </div>
+
+    </div><!-- /.container -->
 </body>
 </html>
